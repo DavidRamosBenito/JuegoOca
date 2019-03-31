93902da8424a4e9baa1050bfa53ee1aa7849a7e9
David <davidramosbenito@hotmail.com>
2019-03-31 17:50:55 +0200

    2ª Instantánea (Clase Dado2 creada y Tablero modificada)

---
+++ src/org/vipsion/oca/modelo/Dado2.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.vipsion.oca.modelo;
+
+/**
+ *
+ * @author David
+ */
+public class Dado2 extends Casilla{
+    
+    public Dado2(int posicion) {
+        super(posicion);
+    }
+    
+    @Override
+    public void posaFicha(Ficha ficha) {
+        
+    }
+}
